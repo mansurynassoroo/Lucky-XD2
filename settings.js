b43c3f911f010e23200ff2434c9ba7960c2d54de
mansurynassoroo <mansurynassoroo@gmail.com>
2025-07-26 00:55:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -106,7 +106,7 @@ OWNER_NAME: process.env.OWNER_NAME || "mansury",
    READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
 
-     DEV: process.env.DEV || "256789966218",
+     DEV: process.env.DEV || "255758547182",
 //replace with your whatsapp number    
     
     ANTI_VV: process.env.ANTI_VV || "true",
